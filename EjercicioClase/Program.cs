1799c9a0224e557cd21858ac2099ef91d6fde271
JonathanLL2811 <156973028+JonathanLL2811@users.noreply.github.com>
2024-01-24 10:30:29 -0600

    Agregar archivos de proyecto.

---
+++ EjercicioClase/Program.cs
@@ -0,0 +1,22 @@
+﻿using EjercicioClase;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        SuperHeroe heroe1 = new SuperHeroe();
+        heroe1.crearheroe("bruce", "batman", "gotica", "No");
+        Console.WriteLine("Nombre: " + heroe1.Nombre);
+        Console.WriteLine("ID secreta: " + heroe1.IdentidadSecreta);
+        Console.WriteLine("Ciudad: " + heroe1.Ciudad);
+        Console.WriteLine("Puede Volar?: " + heroe1.PuedeVolar);
+        Superpoder superpoder1 = new Superpoder();
+        superpoder1.crearpoder("RayosX", "Expulsa rayos cruzados por los ojos", "10");
+        Console.WriteLine("Super Poder: " + superpoder1.Nombre);
+        Console.WriteLine("Descripcion: "+ superpoder1.Descripcion);
+        Console.WriteLine("Nivel: " + superpoder1.Nivel);
+        Console.ReadLine();
+
+
+    }
+}
---
+++ EjercicioClase/Superpoder.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioClase
+{
+    public class Superpoder
+    {
+        string nombre;
+        string descripcion;
+        string nivel;
+
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string Descripcion { get => descripcion; set => descripcion = value; }
+        public string Nivel { get => nivel; set => nivel = value; }
+
+        public void crearpoder(string _nombre, string _descripcion,string _nivel)
+        { 
+            nombre = _nombre;
+            descripcion= _descripcion; ;
+            nivel = _nivel;
+        
+        }
+
+    }
+}
