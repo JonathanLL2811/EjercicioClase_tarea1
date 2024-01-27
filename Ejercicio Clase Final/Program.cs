41177e168e22ddad6e4c1e218f625a88b8dc9e47
JonathanLL2811 <156973028+JonathanLL2811@users.noreply.github.com>
2024-01-27 00:09:01 -0600

    Add files via upload
    Ejercicio Final, completo. 27-1-24

---
+++ Ejercicio Clase Final/Program.cs
@@ -0,0 +1,50 @@
+﻿using EjercicioClase;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        SuperHeroe heroe1 = new SuperHeroe();
+        heroe1.crearheroe("bruce", "batman", "gotica", "No");
+        Console.WriteLine("Nombre: " + heroe1.Nombre);
+        Console.WriteLine("ID secreta: " + heroe1.IdentidadSecreta);
+        Console.WriteLine("Ciudad: " + heroe1.Ciudad);
+        Console.WriteLine("Puede Volar?: " + heroe1.PuedeVolar);
+        Superpoder superpoder1 = new Superpoder();
+        superpoder1.crearpoder("RayosX", "Expulsa rayos cruzados por los ojos", "10");
+        Console.WriteLine("Super Poder: " + superpoder1.Nombre);
+        Console.WriteLine("Descripcion: "+ superpoder1.Descripcion);
+        Console.WriteLine("Nivel: " + superpoder1.Nivel);
+
+        Console.WriteLine("");
+
+        SuperHeroe heroe2 = new SuperHeroe();
+        heroe2.crearheroe("Steve", "Capitan America", "Bruklin", "No");
+        Console.WriteLine("Nombre: " + heroe2.Nombre);
+        Console.WriteLine("ID secreta: " + heroe2.IdentidadSecreta);
+        Console.WriteLine("Ciudad: " + heroe2.Ciudad);
+        Console.WriteLine("Puede Volar?: " + heroe2.PuedeVolar);
+        Superpoder superpoder2 = new Superpoder();
+        superpoder2.crearpoder("super fuerte", "Tiene un escudo", "10");
+        Console.WriteLine("Super Poder: " + superpoder2.Nombre);
+        Console.WriteLine("Descripcion: " + superpoder2.Descripcion);
+        Console.WriteLine("Nivel: " + superpoder2.Nivel);
+
+        Console.WriteLine("");
+
+        SuperHeroe heroe3 = new SuperHeroe();
+        heroe3.crearheroe("Tony Stark", "IroMan", "U.S.A", "Si");
+        Console.WriteLine("Nombre: " + heroe3.Nombre);
+        Console.WriteLine("ID secreta: " + heroe3.IdentidadSecreta);
+        Console.WriteLine("Ciudad: " + heroe3.Ciudad);
+        Console.WriteLine("Puede Volar?: " + heroe3.PuedeVolar);
+        Superpoder superpoder3 = new Superpoder();
+        superpoder3.crearpoder("Es millonario e Inteligente", "Un super traje", "10");
+        Console.WriteLine("Super Poder: " + superpoder3.Nombre);
+        Console.WriteLine("Descripcion: " + superpoder3.Descripcion);
+        Console.WriteLine("Nivel: " + superpoder3.Nivel);
+
+        Console.ReadLine();
+
+    }
+}
---
+++ Ejercicio Clase Final/SuperHeroe.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioClase
+{
+    public class SuperHeroe
+    {
+        private string nombre;
+        private string identidadSecreta;
+        private string ciudad;
+        private string puedeVolar;
+
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string IdentidadSecreta { get => identidadSecreta; set => identidadSecreta = value; }
+        public string Ciudad { get => ciudad; set => ciudad = value; }
+        public string PuedeVolar { get => puedeVolar; set => puedeVolar = value; }
+
+        public void crearheroe(string _nombre,string _identidaSecreta,string _ciudad, string _puedeVolar) {
+            nombre = _nombre;
+            identidadSecreta = _identidaSecreta;
+            ciudad = _ciudad;
+            puedeVolar = _puedeVolar;
+
+
+        
+        }
+}
+
+}
+
+
+
+
+
